744efec56acc2307878081f2022048f0a03ea059
Stonehagen <steinhagen@mailbox.org>
2020-06-28 23:54:05 +0200

    deleted empty lines

--- public/js/app.js
+++ public/js/app.js
@@ -3,22 +3,19 @@ const search = document.querySelector('input')
 const messageOne = document.querySelector('#messageOne')
 const messageTwo = document.querySelector('#messageTwo')
 
-
-
-
 weatherform.addEventListener('submit', (e) => {
-    messageOne.textContent = 'Loading...'
-    messageTwo.textContent = ''
-    e.preventDefault()
-    const location = search.value
-    fetch('http://localhost:3000/weather?address=' + location).then((res) => {
-        res.json().then((data) => {
-            if (data.error) {
-                messageTwo.textContent = data.error
-            } else {
-                messageOne.textContent = data.location
-                messageTwo.textContent = data.forecast
-            }   
-        })
-    })
+	messageOne.textContent = 'Loading...'
+	messageTwo.textContent = ''
+	e.preventDefault()
+	const location = search.value
+	fetch('http://localhost:3000/weather?address=' + location).then((res) => {
+		res.json().then((data) => {
+			if (data.error) {
+				messageTwo.textContent = data.error
+			} else {
+				messageOne.textContent = data.location
+				messageTwo.textContent = data.forecast
+			}
+		})
+	})
 })
